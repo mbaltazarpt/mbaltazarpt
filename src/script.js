20b6d6c4652b26500596be25b881e67ec61b62fc
João Alpalhão <5016958-joaoalpalhao@users.noreply.gitlab.com>
2022-09-26 23:17:57 +0100

    Added more content

--- src/script.js
+++ src/script.js
@@ -2,4 +2,4 @@ const menuButton = document.getElementById('menuButton');
 const menu = document.getElementById('menu');
 const collapse = new Collapse(menu);
 
-menuButton.addEventListener("click", () => collapse.toggle());
+menuButton.addEventListener('click', () => collapse.toggle());
